525e027648a9d1028706f615b5c02a65e84b1e14
MarsonShine <ms27946@163.com>
2018-10-09 00:41:44 +0800

    fix:arraystack pop push 2.add readme

---
+++ Stacks/ArrayStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stacks
+{
+    public class ArrayStack<T>
+    {
+        private T[] items;  //数组
+        private int count;  //栈元素中的个数
+        private int n;  //数组大小
+
+        public ArrayStack(int n)
+        {
+            this.items = new T[n];
+            this.n = n;
+            this.count = 0;
+        }
+
+        //push
+        public bool Push(T item)
+        {
+            //数组控件不够，入栈失败
+            if (count == n) return false;
+            //入栈
+            items[count] = item;
+            ++count;
+            return true;
+        }
+
+        //pop
+        public T Pop()
+        {
+            if (count == 0) return default(T);
+            T item = items[count - 1];
+            --count;
+            return item;
+        }
+    }
+}
