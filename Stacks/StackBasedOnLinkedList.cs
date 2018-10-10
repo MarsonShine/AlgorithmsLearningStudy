b661a049e240d53013e9d623cea155dee1780788
祝琴 <summer@zhuqindeMacBook-Pro.local>
2018-10-11 01:25:02 +0800

    fix:增加基于链式栈结构

---
+++ Stacks/StackBasedOnLinkedList.cs
@@ -0,0 +1,41 @@
+namespace Stacks {
+    /// <summary>
+    /// 基于链表实现的栈
+    /// </summary>
+    public class StackBasedOnLinkedList {
+        private Node top = null;
+        public void Push (int value) {
+            Node newNode = new Node (value, null);
+            //判断栈是否为空
+            if (top == null) {
+                top = newNode;
+            } else {
+                newNode.Next = top;
+                top = newNode;
+            }
+        }
+        /// <summary>
+        /// 用-1表示栈中没有数据
+        /// </summary>
+        /// <returns></returns>
+        public int Pop(){
+            if(top == null) return -1;
+            int value = top.Data;
+            top = top.Next;
+            return value;
+        }
+        private class Node {
+            private int data;
+            private Node next;
+            public Node (int data, Node next) {
+                this.data = data;
+                this.next = next;
+            }
+            public int Data {
+                get { return data; }
+            }
+
+            public Node Next { get { return next; } set { next = value; } }
+        }
+    }
+}
