cab6998d0fe4da9032c48161a84357e798cb937b
祝琴 <summer@zhuqindeMacBook-Pro.local>
2018-10-13 03:25:40 +0800

    fix:递归，循环，尾递归演变分析与性能比较

--- Recursively/Program.cs
+++ Recursively/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Recursively {
     class Program {
@@ -10,9 +12,35 @@ namespace Recursively {
             Console.WriteLine ("FibonacciRecurisivelyAvoidRepeat:" +
                 FibonacciRecurisivelyAvoidRepeat (10));
             Console.WriteLine (FabonacciTailRecurisively (10, 0, 1));
+            Console.WriteLine ("FibonacciContinuation:" + FibonacciContinuation (10, x => x));
             int[] source = { 1, 4, 2, 6, 7, 25369, 22, 14, 55, 63, 785, 1532, 3338 };
             Console.WriteLine (Max (source));
             Console.WriteLine (MaxTailRecurisiveliy (source, 0, 1));
+
+            Console.WriteLine ("阶乘：" + FactorialGeneral (10));
+            Console.WriteLine (FactorialRecurisively (10));
+            Console.WriteLine (FactorialTailRecurisively (10, 1, 1));
+            Console.WriteLine (FactorialContinuation (10, x => x));
+
+            Console.WriteLine ("---------性能比较---------");
+            Stopwatch sw = new Stopwatch ();
+            int deep = 40;
+            sw.Start ();
+            var r = FabonacciRecurisively (deep);
+            sw.Stop ();
+            Console.WriteLine ("value=" + r + " 递归:" + sw.ElapsedMilliseconds + "毫秒");
+            sw.Restart ();
+            r = FibonacciRecurisivelyAvoidRepeat (deep);
+            sw.Stop ();
+            Console.WriteLine ("value=" + r + " 避免重复计算:" + sw.ElapsedMilliseconds + "毫秒");
+            sw.Restart ();
+            r = Fibonacci (deep);
+            sw.Stop ();
+            Console.WriteLine ("value=" + r + " 循环体：" + sw.ElapsedMilliseconds + "毫秒");
+            sw.Restart ();
+            r = FibonacciTialRecurisively2 (deep, 0, 1);
+            sw.Stop ();
+            Console.WriteLine ("value=" + r + " 尾递归:" + sw.ElapsedMilliseconds + "毫秒");
         }
         public static int Fanonacci (int n, int acc1, int acc2) {
             while (n != 0) {
@@ -92,6 +120,60 @@ namespace Recursively {
             if (max < source[i])
                 max = source[i];
             return MaxTailRecurisiveliy (source, max, i + 1);
+        }
+
+        private List<Menu> m_menus;
+        public static Menu GetHighestTopParentMenuGeneral (List<Menu> menus, Menu target) {
+            if (target.ParentId == 0) return target;
+            while (target.ParentId != 0) {
+                var middel = menus.FirstOrDefault (m => m.Id == target.ParentId);
+                target = middel;
+            }
+            return target;
+        }
+
+        public static Menu GetHighestTopParentMenuRecurisively (List<Menu> source, Menu target) {
+            if (target.ParentId == 0) return target;
+            return GetHighestTopParentMenuRecurisively (source, source.First (m => m.Id == target.ParentId));
+        }
+
+        public static int FactorialRecurisively (int n) {
+            if (n == 0) return 1;
+            return FactorialRecurisively (n - 1) * n;
+        }
+
+        public static int FactorialGeneral (int n) {
+            int ret = 1;
+            var i = 1;
+            if (n == 0) return ret;
+            while (i < n) {
+                var prev = (i + 1);
+                ret = ret * prev;
+                i++;
+            }
+            return ret;
+        }
+
+        public static int FactorialTailRecurisively (int n, int ret, int i) {
+            if (n == 0) return ret;
+            return FactorialTailRecurisively (n - 1, ret * i, i + 1);
+        }
+
+        //continuation
+        public static int FactorialRecurisively2 (int n) {
+            return FactorialContinuation (n - 1, r => n * r);
+        }
+
+        public static int FactorialContinuation (int n, Func<int, int> continuation) {
+            if (n == 0) return continuation (1);
+            return FactorialContinuation (n - 1, r => continuation (n * r));
+        }
+
+        public static int FibonacciContinuation (int n, Func<int, int> continuation) {
+            if (n < 2) return continuation (n);
+            return FibonacciContinuation (n - 1,
+                r1 => FibonacciContinuation (n - 2,
+                    r2 => continuation (r1 + r2)));
         }
     }
 }
