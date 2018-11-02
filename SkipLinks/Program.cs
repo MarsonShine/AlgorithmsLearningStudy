6dd2f1e2bcd098a0c2809f95fd1300ea38500482
Shine Marson <ms27946@outlook.com>
2018-11-02 09:13:31 +0800

    feature：跳表

---
+++ SkipLinks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SkipLinks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
