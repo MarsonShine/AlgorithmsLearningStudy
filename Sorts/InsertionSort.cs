ba9ea648cd0203acf28b1f1fbb4b20acf471059c
Shine Marson <ms27946@outlook.com>
2018-11-29 16:40:52 +0800

    fix:插入排序注释

--- Sorts/InsertionSort.cs
+++ Sorts/InsertionSort.cs
@@ -1,11 +1,11 @@
 namespace Sorts {
     public class InsertionSort {
-        public void Sort (int[] a) {
+        public void Sort(int[] a) {
             if (a.Length < 1) return;
-            for (int i = 1; i < a.Length; i++) {
+            for (int i = 1; i < a.Length; i++) { //未排序集合
                 int value = a[i];
                 int j = i - 1;
-                for (; j >= 0; j--) {
+                for (; j >= 0; j--) { //已排序集合
                     if (a[j] > value) {
                         //移动数据
                         a[j + 1] = a[j];
