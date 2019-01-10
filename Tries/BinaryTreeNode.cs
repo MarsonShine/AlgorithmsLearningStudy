ca8f6ab1cb995b8028b9f946bf6eac54400a800b
祝琴 <summer@zhuqindeMacBook-Pro.local>
2019-01-11 00:46:25 +0800

    feature：Trie 树字符串匹配算法深入理解

---
+++ Tries/BinaryTreeNode.cs
@@ -0,0 +1,8 @@
+namespace Tries {
+    public class BinaryTreeNode {
+        public char Data { get; set; }
+        public BinaryTreeNode Left { get; set; }
+        public BinaryTreeNode Right { get; set; }
+
+    }
+}
