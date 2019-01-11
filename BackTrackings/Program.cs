8f63481688478747cf582ce2151ac107a0a30348
Shine Marson <ms27946@outlook.com>
2019-01-11 12:49:52 +0800

    feature:回溯算法，八皇后问题

---
+++ BackTrackings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackTrackings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
