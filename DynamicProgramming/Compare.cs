ef2a18ed7e3e3f51dd845046e305bf8c0b76634b
Shine Marson <ms27946@outlook.com>
2019-01-15 12:37:44 +0800

    feature：动态规划思想

---
+++ DynamicProgramming/Compare.cs
@@ -0,0 +1,14 @@
+namespace DynamicProgramming {
+    /// <summary>
+    /// 比较 01背包问题的回溯算法与动态规划
+    /// </summary>
+    public class Compare {
+        private int _maxWeight = int.MinValue; //存储包中物品的最大值
+        private int[] _weightArray = new int[] { 2, 2, 4, 6, 3 }; //物品重量
+        private int _packageCount = 5; //物品个数
+        private int _maxWeightInPackage = 9; //背包承受的最大重量
+        public void BackTracking() {
+
+        }
+    }
+}
---
+++ DynamicProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DynamicProgramming {
+    /// <summary>
+    /// 动态规划核心思想就是把问题分解成多个小问题
+    /// </summary>
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
