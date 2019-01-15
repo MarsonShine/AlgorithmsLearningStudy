04fc71e97f4f189af253be6d1d943e61dd15702e
祝琴 <summer@zhuqindeMacBook-Pro.local>
2019-01-16 00:34:22 +0800

    feature:AC自动机，多模式字符串匹配算法实现字符串过滤功能

---
+++ AhoCorasikes/AcNode.cs
@@ -0,0 +1,11 @@
+namespace AhoCorasikes {
+    public class AcNode {
+        public char Data { get; set; }
+        public AcNode[] children = new AcNode[26]; //假设字符串只包含这26个字母
+        public bool IsEndingChar = false; //结尾字符为 true
+        public int length = -1; //当IsEndingChar=true时，记录模式串长度
+        public AcNode(char data) {
+            this.Data = data;
+        }
+    }
+}
---
+++ AhoCorasikes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AhoCorasikes {
+    /// <summary>
+    /// AC多模式串匹配算法，广泛用于字符串过滤
+    /// </summary>
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
