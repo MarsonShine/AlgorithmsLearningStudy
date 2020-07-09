4e22c4ac2f3d327efd681241e4b7cbf7e34a9ac8
maoshuai <maoshuai@kingsunsoft.com>
2020-07-09 13:50:06 +0800

    fix: 最短路径问题算法

---
+++ DynamicProgramming/ShortestPathProblem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicProgramming
+{
+    // 最短路径问题
+    // 从A点到G点的最短路径
+    public class ShortestPathProblem
+    {
+        public static int MinPath(int[,] matrix)
+        {
+            return Process(matrix, matrix.GetLength(1) - 1);
+        }
+
+        private static int Process(int[,] matrix, int i)
+        {
+            if (i == 0)
+                return 0;
+            // 状态转移方程
+            else
+            {
+                int distance = int.MaxValue;
+                for (int j = 0; j < i; j++)
+                {
+                    if (matrix[j, i] != 0)
+                    {
+                        int temp = matrix[j, i] + Process(matrix, j);
+                        if (temp < distance)
+                        {
+                            distance = temp;
+                        }
+                    }
+                }
+                return distance;
+            }
+        }
+    }
+}
