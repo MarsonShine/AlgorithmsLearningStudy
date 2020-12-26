f1bb1654a9316af8d5e8d26dec987f6656f9d56d
marsonshine <ms27946@163.com>
2020-12-26 14:36:41 +0800

    fix: totp 两步验证算法

---
+++ Encryptions/TotpAuthenticationService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryptions
+{
+    /// <summary>
+    /// TOTP:https://zh.wikipedia.org/wiki/%E5%9F%BA%E4%BA%8E%E6%97%B6%E9%97%B4%E7%9A%84%E4%B8%80%E6%AC%A1%E6%80%A7%E5%AF%86%E7%A0%81%E7%AE%97%E6%B3%95
+    /// 结合私钥和当前时间戳，在通过 HMAC 加密生成一次性密码
+    /// 时间戳会因不同服务器的系统时间的不同要设置一个偏差量，wiki 上默认是 30 秒
+    /// 微软自带实现的 TOCP 默认时间差是 3 分钟
+    /// 参考自:https://github.com/aspnet/AspNetIdentity/blob/master/src/Microsoft.AspNet.Identity.Core/Rfc6238AuthenticationService.cs
+    /// </summary>
+    public class TotpAuthenticationService
+    {
+        private static readonly DateTime _unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        private static readonly TimeSpan _timestep = TimeSpan.FromMinutes(3);
+        private static readonly Encoding _encoding = new UTF8Encoding(false, true);
+
+        public static int GenerateCode(byte[] securityToken, string modifier = null)
+        {
+            if (securityToken == null)
+            {
+                throw new ArgumentNullException(nameof(securityToken));
+            }
+
+            // Allow a variance of no greater than 9 minutes in either direction
+            var currentTimeStep = GetCurrentTimeStepNumber();
+            using (var hashAlgorithm = new HMACSHA1(securityToken))
+            {
+                return ComputeTotp(hashAlgorithm, currentTimeStep, modifier);
+            }
+        }
+        /// <summary>
+        /// 通过顶一个纪元(epoch)的起点与时间步长(time step)为单位计数
+        /// 转换公式见 wiki
+        /// </summary>
+        /// <returns>TC = 最低值((unixtime(当前时间)−unixtime(T0))/TS)</returns>
+        private static ulong GetCurrentTimeStepNumber()
+        {
+            var delta = DateTime.UtcNow - _unixEpoch;
+            return (ulong)(delta.Ticks / _timestep.Ticks);
+        }
+
+        private static int ComputeTotp(HashAlgorithm hashAlgorithm, ulong timestepNumber, string modifier)
+        {
+            // # of 0's = length of pin
+            const int mod = 1000000;
+
+            // See https://tools.ietf.org/html/rfc4226
+            // We can add an optional modifier
+            var timestepAsBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder((long)timestepNumber));
+            var hash = hashAlgorithm.ComputeHash(ApplyModifier(timestepAsBytes, modifier));
+
+            // Generate DT string
+            // 取最高位
+            var offset = hash[hash.Length - 1] & 0xf;
+            Debug.Assert(offset + 4 < hash.Length);
+            // 取其中四位,按位补零
+            var binaryCode = (hash[offset] & 0x7f) << 24
+                             | (hash[offset + 1] & 0xff) << 16
+                             | (hash[offset + 2] & 0xff) << 8
+                             | (hash[offset + 3] & 0xff);
+
+            return binaryCode % mod;
+        }
+
+        private static byte[] ApplyModifier(byte[] input, string modifier)
+        {
+            if (String.IsNullOrEmpty(modifier))
+            {
+                return input;
+            }
+
+            var modifierBytes = _encoding.GetBytes(modifier);
+            var combined = new byte[checked(input.Length + modifierBytes.Length)];
+            Buffer.BlockCopy(input, 0, combined, 0, input.Length);
+            Buffer.BlockCopy(modifierBytes, 0, combined, input.Length, modifierBytes.Length);
+            return combined;
+        }
+    }
+}
