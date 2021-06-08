237885db260cfdab777a2c1d9fcb9ce7b6df1a75
marsonshine <ms27946@163.com>
2021-06-08 13:39:02 +0800

    doc: 最长连续有效字符

---
+++ docs/Stack-Leecode-LongestValidParentheses.md
@@ -0,0 +1,4 @@
+# 栈相关操作算法——求最长有效括号
+
+https://leetcode-cn.com/problems/longest-valid-parentheses/
+
--- go/leecode32.go
+++ go/leecode32.go
@@ -3,51 +3,31 @@ package main
 func longestValidParentheses(s string) int {
 	// 遍历
 	l := len(s)
-	c := 0
+	notmatch := []int{}
+	notmatch = append(notmatch, -1)
+	maxLength := 0
+
 	arr := []byte{}
-	// left := byte('(')  // (
+	left := byte('(')  // (
 	right := byte(')') // )
 	for i := 0; i < l; i++ {
-		if s[i] == byte(right) {
-			if len(arr) == 0 {
-				continue
+		arrl := len(arr)
+		if s[i] == right {
+			if arrl != 0 && arr[arrl-1] == left {
+				arr = arr[:arrl-1]
+				notmatch = notmatch[:len(notmatch)-1]
+				longLength := i - notmatch[len(notmatch)-1]
+				if maxLength < longLength {
+					maxLength = longLength
+				}
+			} else {
+				arr = append(arr, s[i])
+				notmatch = append(notmatch, i)
 			}
-			arr = arr[:len(arr)-1]
-			c += 2
 		} else {
 			arr = append(arr, s[i])
+			notmatch = append(notmatch, i)
 		}
 	}
-	return c
+	return maxLength
 }
-
-// func longestValidParentheses(s string) int {
-// 	maxAns := 0
-// 	dp := make([]int, len(s))
-// 	for i := 1; i < len(s); i++ {
-// 		if s[i] == ')' {
-// 			if s[i-1] == '(' {
-// 				if i >= 2 {
-// 					dp[i] = dp[i-2] + 2
-// 				} else {
-// 					dp[i] = 2
-// 				}
-// 			} else if i-dp[i-1] > 0 && s[i-dp[i-1]-1] == '(' {
-// 				if i-dp[i-1] >= 2 {
-// 					dp[i] = dp[i-1] + dp[i-dp[i-1]-2] + 2
-// 				} else {
-// 					dp[i] = dp[i-1] + 2
-// 				}
-// 			}
-// 			maxAns = max(maxAns, dp[i])
-// 		}
-// 	}
-// 	return maxAns
-// }
-
-// func max(x, y int) int {
-// 	if x > y {
-// 		return x
-// 	}
-// 	return y
-// }
