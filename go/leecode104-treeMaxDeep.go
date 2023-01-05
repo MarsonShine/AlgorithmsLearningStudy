7e58ac8731dbff713bbcfcd85680abb9226e2dfb
marsonshine <ms27946@outlook.com>
2023-01-06 01:18:32 +0800

    feat: level order of max depth binarytree

--- go/leecode104-treeMaxDeep.go
+++ go/leecode104-treeMaxDeep.go
@@ -16,3 +16,27 @@ func deep(node *TreeNode, depth int) int {
 	}
 	return maxdepth
 }
+
+// 层序遍历
+func maxDepth2(root *TreeNode) int {
+	queue := []*TreeNode{}
+	max := 0
+	if root != nil {
+		queue = append(queue, root)
+	}
+	for len(queue) > 0 {
+		length := len(queue)
+		for i := 0; i < length; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		max += 1
+	}
+	return max
+}
