a749f2171c56e3038cd43762c7288e9e01418c2b
marsonshine <ms27946@outlook.com>
2023-01-07 00:36:21 +0800

    fix: level order; bsd,dfs

--- go/leecode226-invertTree.go
+++ go/leecode226-invertTree.go
@@ -17,11 +17,49 @@ func invertTreeRecurison(node *TreeNode) {
 		return
 	}
 	if node.Left != nil || node.Right != nil {
-		left := node.Left
-		right := node.Right
-		node.Right = left
-		node.Left = right
+		node.Left, node.Right = node.Right, node.Left
 	}
 	invertTreeRecurison(node.Left)
 	invertTreeRecurison(node.Right)
+}
+
+// 迭代，深度遍历，前序遍历
+func invertTree2(root *TreeNode) *TreeNode {
+	stack := []*TreeNode{}
+	node := root
+	for node != nil || len(stack) > 0 {
+		for node != nil {
+			node.Left, node.Right = node.Right, node.Left
+			stack = append(stack, node)
+			node = node.Left
+		}
+		node = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		node = node.Right
+	}
+	return root
+}
+
+// 广度优先算法
+// 层序遍历
+func invertTree3(root *TreeNode) *TreeNode {
+	queue := []*TreeNode{}
+	if root != nil {
+		queue = append(queue, root)
+	}
+	for len(queue) > 0 {
+		length := len(queue)
+		for i := 0; i < length; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+			node.Left, node.Right = node.Right, node.Left
+		}
+	}
+	return root
 }
