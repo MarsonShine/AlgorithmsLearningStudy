cc19654fc6f3089d3cb7f437c946eae706f43078
marsonshine <ms27946@163.com>
2023-01-31 20:11:38 +0800

    fix: iterator，has path sum

--- go/leecode112-hasPathSum.go
+++ go/leecode112-hasPathSum.go
@@ -15,3 +15,27 @@ func hasPathSumRecurision(root *TreeNode, targetSum int) bool {
 	r := hasPathSumRecurision(root.Right, targetSum-root.Val)
 	return l || r
 }
+
+func hasPathSum2(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	stack := make([]*TreeNode, 0)
+	stack = append(stack, root)
+	ret := 0
+	// 因为要回溯，所以要记录每个节点的累加值
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if node.Left == nil && node.Right == nil {
+			ret += node.Val
+			if targetSum == ret {
+				return true
+			}
+		}
+		if node.Left != nil {
+
+		}
+	}
+	return false
+}
