1d6b0f0a77209714f853f2bb5bcf5c084fa3314d
marsonshine <ms27946@163.com>
2024-08-29 21:24:01 +0800

    feat:random weight selector

---
+++ csharp/Randoms/WeightedRandom/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WeightedRandom;
+
+Console.WriteLine("Hello, World!");
+
+var itemWeights = new Dictionary<string, double>
+{
+	{ "A", 100 },
+	{ "B", 80 },
+	{ "C", 60 }
+};
+var selector = new WeightedRandomSelector<string>(itemWeights);
+var selections = new Dictionary<string, int>();
+int totalSelections = 1000000;
+
+// Act
+for (int i = 0; i < totalSelections; i++)
+{
+	string selected = selector.Select();
+	if (!selections.ContainsKey(selected))
+	{
+		selections[selected] = 0;
+	}
+	selections[selected]++;
+}
+
+// Assert
+double totalWeight = itemWeights.Values.Sum();
+foreach (var item in itemWeights)
+{
+	double expectedProbability = item.Value / totalWeight;
+	double actualProbability = (double)selections[item.Key] / totalSelections;
+	var delta = Math.Abs(expectedProbability - actualProbability);
+	Console.WriteLine($"Item {item.Key} was selected with probability {actualProbability}, " +
+		$"expected {expectedProbability}, delta {delta}");
+}
---
+++ csharp/Randoms/WeightedRandom/WeightedRandomSelector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightedRandom
+{
+	internal class WeightedRandomSelector<T>(Dictionary<T, double> itemWeights)
+		where T : notnull
+	{
+		private readonly Random _random = new Random();
+		private readonly Dictionary<T, double> _itemWeights = itemWeights;
+
+		public T Select()
+		{
+			double totalWeight = _itemWeights.Values.Sum();
+			double randomWeight = _random.NextDouble() * totalWeight;
+			double weightSum = 0;
+
+			foreach (var item in _itemWeights)
+			{
+				weightSum += item.Value;
+				if (randomWeight <= weightSum)
+				{
+					return item.Key;
+				}
+			}
+
+			throw new InvalidOperationException("No item selected");
+		}
+	}
+}
